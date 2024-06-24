abd5d29f34d85e58a6412e7fb96294df6a0b7680
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-24 21:02:21 +0300

    Update dir.py

--- MatrixMusic/core/dir.py
+++ MatrixMusic/core/dir.py
@@ -17,4 +17,4 @@ def dirr():
     if "cache" not in os.listdir():
         os.mkdir("cache")
 
-    LOGGER("ميوزك العالم").info("تم تحـديث السـورس ...✓")
+    LOGGER("ميوزك wolf").info("تم تحـديث السـورس ...✓")
