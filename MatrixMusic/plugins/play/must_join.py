896eb86e9d9f05dc3640c65ad1a785245937fa4e
Fox455664 <118382354+Fox455664@users.noreply.github.com>
2024-06-24 19:15:51 +0300

    Update must_join.py

--- MatrixMusic/plugins/play/must_join.py
+++ MatrixMusic/plugins/play/must_join.py
@@ -5,11 +5,11 @@ from MatrixMusic import app
 
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(bot: Client, msg: Message):
-    if not "https://t.me/vzo_a":  # Not compulsory
+    if not "https://t.me/fox68899":  # Not compulsory
         return
     try:
         try:
-            await bot.get_chat_member("vzo_a", msg.from_user.id)
+            await bot.get_chat_member("fox68899", msg.from_user.id)
         except UserNotParticipant:
             if "https://t.me/vzo_a".isalpha():
                 link = "https://t.me/vzo_a"
@@ -18,14 +18,14 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @vzo_a .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
+                    f"⌯︙عذࢪاَ عزيزي ↫ {msg.from_user.mention} \n⌯︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ\n⌯︙قناة البوت: @fox68899 .\nꔹ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ꔹ",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("سـورس الـعالم", url=link)]
+                        [InlineKeyboardButton("سـورس wolf", url=link)]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I m not admin in the MUST_JOIN chat @vzo_a !")
+        print(f"I m not admin in the MUST_JOIN chat @fox68899 !")
